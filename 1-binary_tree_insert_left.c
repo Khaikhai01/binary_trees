27edfce4a8052377f8cd6af7f84595560cb843da
Joseph Kakai <josephngalu96gmail.com>
2023-02-28 15:19:10 +0300

    merge

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,11 +7,10 @@
  * Return: null on failure and the node on success
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
-{
-	binary_tree_t *NewNode = binary_tree_node(parent, value);
+{	
 	binary_tree_t *OldNode = NULL;
 
-	if (parent == NULL || NewNode == NULL) 
+	if (parent == NULL) 
 	{
 		return (NULL);
 	}
