3c062c87bc385d7c3a0eb8906fef7e3c8334de7e
Khaikhai01 <shishchei@gmail.com>
2023-02-28 15:07:34 +0300

    task 2

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - insert a node to the right
+ * @parent: parent of the node
+ * @value: value of the node
+ * Return: null on failure and the node on success
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *OldNode = NULL;
+
+	if (parent == NULL) 
+	{
+		return (NULL);
+	}
+	if (parent->right == NULL)
+	{
+		parent->right = binary_tree_node(parent, value    );
+	}
+	else
+	{
+		OldNode = parent->right;
+		parent->right = binary_tree_node(parent, value);
+		parent->right->right = OldNode;
+		OldNode->parent = parent->right;
+	}
+	return (parent->right);
+}	
