1d2484d1a6e20e86b54bffc446a800495ac7d143
Joseph Kakai <josephngalu96gmail.com>
2023-02-28 23:30:10 +0300

    task 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -6,12 +6,13 @@
  * Return: 1
  */
 
-int binary_tree_is_root(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (!node)
-	return (0);
+		return (0);
 
 	if(!node->parent)
 		return (1);
+	
 	return (0);
 }
