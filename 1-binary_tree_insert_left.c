c0b0589451c9d03506a8c5280bc37f8c09c83960
Wanton Raven <49359467+josephkakai@users.noreply.github.com>
2023-02-28 15:16:27 +0300

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,17 +1,15 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_insert_left - insert a node to the left
- * @parent: parent of the node
- * @value: value of the node
- * Return: null on failure and the node on success
- */
+* binary_tree_insert_left - insert a node to the left
+* @parent: parent of the node
+* @value: value of the node
+* Return: Null on failure and the node on success
+*/
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *NewNode = binary_tree_node(parent, value);
 	binary_tree_t *OldNode = NULL;
 
-	if (parent == NULL || NewNode == NULL) 
+	if (parent == NULL)
 	{
 		return (NULL);
 	}
@@ -27,4 +25,4 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		OldNode->parent = parent->left;
 	}
 	return (parent->left);
-}	
+}
