2eee495eb0b3fafc41316abd109b723106f09b83
Khaikhai01 <shishchei@gmail.com>
2023-02-28 15:13:32 +0300

    task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -4,7 +4,8 @@
  * binary_tree_insert_right - insert a node to the right
  * @parent: parent of the node
  * @value: value of the node
- * Return: null on failure and the node on success
+ * Return: null on failure 
+ 	   the node on success
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
