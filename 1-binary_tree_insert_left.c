5655c2c7b2b462d1319c9d27f50170957271b381
Khaikhai01 <shishchei@gmail.com>
2023-02-28 17:04:47 +0300

    insertion into a tree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,7 +6,7 @@
 * Return: Null on failure and the node on success
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
-{	
+{
 	binary_tree_t *OldNode = NULL;
 
 	if (parent == NULL)
