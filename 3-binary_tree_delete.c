dc83774345c34071f151bca540e9de56046f7d60
Joseph Kakai <josephngalu96gmail.com>
2023-02-28 21:35:21 +0300

    updated

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes a tree from binry tree
+ * @tree - the tree to delete
+ *
+ * Return: void
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree ==  NULL)
+		return;
+
+	binary_tree_delete(tree->left); 
+	binary_tree_delete(tree->right);
+
+i	free(tree);
+}
