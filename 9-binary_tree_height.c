aaafeb3367d21492add01111dd445786adcbfd1f
Wanton Raven <49359467+josephkakai@users.noreply.github.com>
2023-03-01 20:39:32 +0300

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,37 +1,34 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the height of a tree
- * @tree: the tree to measure
+ * max - finds maximum height between two paths in a tree
+ * @left: length of left path
+ * @right: length of right path
  *
- * Return: int
+ * Return: max between the two paths
+ */
+size_t max(size_t left, size_t right)
+{
+	if (left < right)
+		return (right);
+	return (left);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to root node of tree to measure height
+ *
+ * Return: height of tree else 0
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t heightleft = 0;
-	size_t heightright = 0;
-	size_t treeheight = 0;
-	
-	if (tree == NULL)
+	size_t left, right;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
-	heightleft = binary_tree_height(tree->left);
-	heightright = binary_tree_height(tree->right);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 
-	treeheight = max(heightleft, heightright) + 1;
-	return (treeheight);
-}
-
-int max(int left, int right)
-{
-	int max = 0;
-	if (left > right)
-	{
-		max = left;
-	}
-	else
-	{	
-		max = right;
-	}
-	return (max);
+	return (max(left, right) + 1);
 }
