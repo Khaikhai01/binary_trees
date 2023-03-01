6e842548c868e8e7511a5554c8dca950314e193a
Khaikhai01 <shishchei@gmail.com>
2023-03-01 21:46:30 +0300

    depth

--- binary_trees.h
+++ binary_trees.h
@@ -35,4 +35,5 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 int max(int left, int right);
+size_t binary_tree_depth(const binary_tree_t *tree);
 #endif
