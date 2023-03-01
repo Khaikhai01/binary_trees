281be97c50885c87fd710f12aa4e344fa036ee76
Joseph Kakai <josephngalu96gmail.com>
2023-03-01 22:49:24 +0300

    Updated to be more efficient

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,11 +8,8 @@
  */
 size_t binary_tree_size(binary_tree_t *tree)
 {
-	size_t size = 0;
-
 	if (!tree)
 		return (0);
 
-	size = binary_tree_size(tree->left) + binary_tree_size(tree->right);
-	return (size + 1);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
