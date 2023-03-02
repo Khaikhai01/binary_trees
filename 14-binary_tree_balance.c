765c2fca8f7644a361bf6f4cbc82abb0a6915de1
Joseph Kakai <josephngalu96gmail.com>
2023-03-02 14:07:09 +0300

    updated error

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -38,7 +38,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  * @tree: The binary tree
  * Return: The bf
  */
-int binary_tree_balance(const binary_tree_t *tree)
+size_t binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
