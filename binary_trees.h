c8d7d379684b62c48d67168c55ac2a0797259005
Joseph Kakai <josephngalu96gmail.com>
2023-03-02 12:06:16 +0300

    corrected header

--- binary_trees.h
+++ binary_trees.h
@@ -37,7 +37,7 @@ size_t binary_tree_height(const binary_tree_t *tree);
 int max(int left, int right);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
-size_t binary_tree_size(binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 #endif
