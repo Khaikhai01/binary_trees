dc3b00287f969febee07244098e7f3415866e871
Joseph Kakai <josephngalu96@gmail.com>
2023-03-02 09:46:59 -0800

    And final my last task

--- binary_trees.h
+++ binary_trees.h
@@ -42,4 +42,5 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 size_t binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 #endif
